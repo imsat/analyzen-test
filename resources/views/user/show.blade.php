a05cbbd4f89323fe527b6e72f50e0a620e7e44bc
imsat <satyajit.cse@yahoo.com>
2024-03-17 04:19:14 +0600

    user address svg update

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -57,36 +57,36 @@
                             </div>
                             <div class="sm:col-span-1">
                                 <dt class="text-sm font-medium text-gray-500 dark:text-white">Avatar</dt>
-                                <dd class="mt-1 text-sm text-gray-900 dark:text-white"><img src="{{$user->avatar}}" class="img-40 mt-2" alt="User avatar"></dd>
+                                <dd class="mt-1 text-sm text-gray-900 dark:text-white"><img src="{{$user->avatar}}"
+                                                                                            class="img-40 mt-2"
+                                                                                            alt="User avatar"></dd>
                             </div>
                             <div class="sm:col-span-1">
                                 <dt class="text-sm font-medium text-gray-500 dark:text-white">Email address</dt>
                                 <dd class="mt-1 text-sm text-gray-900 dark:text-white">{{$user->email}}</dd>
                             </div>
                             @if($user->addresses->count())
-                            <div class="sm:col-span-2">
-                                <dt class="text-sm font-medium text-gray-500 dark:text-white">Address</dt>
-                                <dd class="mt-1 text-sm text-gray-900 dark:text-white">
-                                    <ul role="list" class="divide-y divide-gray-200 rounded-md border border-gray-200">
-                                        @foreach($user->addresses as $address)
-                                            <li class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
-                                                <div class="flex w-0 flex-1 items-center">
-                                                    <!-- Heroicon name: mini/paper-clip -->
-                                                    <svg class="h-5 w-5 flex-shrink-0 text-gray-400"
-                                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                                                         fill="currentColor" aria-hidden="true">
-                                                        <path fill-rule="evenodd"
-                                                              d="M15.621 4.379a3 3 0 00-4.242 0l-7 7a3 3 0 004.241 4.243h.001l.497-.5a.75.75 0 011.064 1.057l-.498.501-.002.002a4.5 4.5 0 01-6.364-6.364l7-7a4.5 4.5 0 016.368 6.36l-3.455 3.553A2.625 2.625 0 119.52 9.52l3.45-3.451a.75.75 0 111.061 1.06l-3.45 3.451a1.125 1.125 0 001.587 1.595l3.454-3.553a3 3 0 000-4.242z"
-                                                              clip-rule="evenodd"/>
-                                                    </svg>
-                                                    <span class="ml-2 w-0 flex-1 truncate">{{$address->name}}</span>
-                                                </div>
-                                            </li>
-                                        @endforeach
-                                    </ul>
-                                </dd>
-                            </div>
-                                @endif
+                                <div class="sm:col-span-2">
+                                    <dt class="text-sm font-medium text-gray-500 dark:text-white">Address</dt>
+                                    <dd class="mt-1 text-sm text-gray-900 dark:text-white">
+                                        <ul role="list"
+                                            class="divide-y divide-gray-200 rounded-md border border-gray-200">
+                                            @foreach($user->addresses as $address)
+                                                <li class="flex items-center justify-between py-3 pl-3 pr-4 text-sm">
+                                                    <div class="flex w-0 flex-1 items-center">
+                                                        <!-- Heroicon name: mini/map-pin -->
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5">
+                                                            <path fill-rule="evenodd" d="m9.69 18.933.003.001C9.89 19.02 10 19 10 19s.11.02.308-.066l.002-.001.006-.003.018-.008a5.741 5.741 0 0 0 .281-.14c.186-.096.446-.24.757-.433.62-.384 1.445-.966 2.274-1.765C15.302 14.988 17 12.493 17 9A7 7 0 1 0 3 9c0 3.492 1.698 5.988 3.355 7.584a13.731 13.731 0 0 0 2.273 1.765 11.842 11.842 0 0 0 .976.544l.062.029.018.008.006.003ZM10 11.25a2.25 2.25 0 1 0 0-4.5 2.25 2.25 0 0 0 0 4.5Z" clip-rule="evenodd" />
+                                                        </svg>
+
+                                                        <span class="ml-2 w-0 flex-1 truncate">{{$address->name}}</span>
+                                                    </div>
+                                                </li>
+                                            @endforeach
+                                        </ul>
+                                    </dd>
+                                </div>
+                            @endif
                         </dl>
                     </div>
                 </div>
